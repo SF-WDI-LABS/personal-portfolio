49ba4496f91104c555d503c61112159ccd7b8050
AdamMenard <adammenard888@gmail.com>
2017-12-03 19:58:11 -0800

    added titles and started array for projects

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log("JS at the Ready!");
+
+$(document).ready(function() {
+
+// Projects array
+let projects = [
+  {
+    id: "1",
+    name:
+    startDate:
+    description:
+    url:
+  },
+  {
+    id: "2",
+    name:
+    startDate:
+    description:
+    url:
+  },
+  {
+    id: "3",
+    name:
+    startDate:
+    description:
+    url:
+  },
+];
+
+// Loop for adding Projects
+for (i = 0; i < projects.length; i++) {
+  $()
+}
+
+)};
